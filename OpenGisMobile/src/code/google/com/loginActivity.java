2c79a3a3df8d33eda8de600968081a8357f43428
jugade92@gmail.com <jugade92@gmail.com@3954e759-7755-a951-c8e7-e4e3e0f8dd14>
2012-01-16 16:57:02 +0000

    Primer commit Android

---
+++ OpenGisMobile/src/code/google/com/loginActivity.java
@@ -0,0 +1,13 @@
+package code.google.com;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class loginActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
