423c6acbc27720eb934aa60614f1cbdda6725b42
jugade92@gmail.com <jugade92@gmail.com@3954e759-7755-a951-c8e7-e4e3e0f8dd14>
2012-01-17 14:45:45 +0000

    Implimentado el botón borrar del login.

--- OpenGisMobile/src/code/google/com/loginActivity.java
+++ OpenGisMobile/src/code/google/com/loginActivity.java
@@ -2,6 +2,9 @@ package code.google.com;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class loginActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +12,26 @@ public class loginActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+        
+        
+        Button cmdBorrar = (Button) findViewById(R.id.cmdBorrar);
+        Button cmdEnviar = (Button) findViewById(R.id.cmdEnviar);
+        final EditText txtUser = (EditText) findViewById(R.id.txtUser);
+        final EditText txtPass = (EditText) findViewById(R.id.txtPass);
+
+        cmdBorrar.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				
+				txtUser.setText("");
+				txtPass.setText("");
+				
+				
+				
+			}
+		});
+        
+        
+        
     }
 }
