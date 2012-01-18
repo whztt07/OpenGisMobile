da96f069a35626f1a29cbd42ae8fc84489b6ebe9
jugade92@gmail.com <jugade92@gmail.com@3954e759-7755-a951-c8e7-e4e3e0f8dd14>
2012-01-18 19:36:25 +0000

    Primer webservice

--- OpenGisMobile/src/code/google/com/loginActivity.java
+++ OpenGisMobile/src/code/google/com/loginActivity.java
@@ -1,6 +1,18 @@
 package code.google.com;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,7 +27,8 @@ public class loginActivity extends Activity {
         
         
         Button cmdBorrar = (Button) findViewById(R.id.cmdBorrar);
-        Button cmdEnviar = (Button) findViewById(R.id.cmdEnviar);    
+        Button cmdEnviar = (Button) findViewById(R.id.cmdEnviar);   
+        
         final EditText txtUser = (EditText) findViewById(R.id.txtUser);
         final EditText txtPass = (EditText) findViewById(R.id.txtPass);
 
@@ -33,5 +46,101 @@ public class loginActivity extends Activity {
         
         
         
+        cmdEnviar.setOnClickListener(new View.OnClickListener(){
+
+			public void onClick(View arg0) {
+				
+				try{
+				
+				
+				String data = getJSONdata("http://79.108.245.167/OpenGisMobile/webService.php");
+				
+				String resultado = parseJSONdata(data);
+				
+				alertaMensaje(resultado,resultado);
+				
+				}catch(Exception e2){
+					
+					alertaMensaje("Esto peta","VAYA");
+					
+				}
+				
+			}
+        	
+        	
+        	
+        });
+        
+        
+        
+        
+        
+        
     }
+    
+    
+    public void alertaMensaje(String cadena,String titulo) {
+      
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
+        dialogBuilder.setMessage(cadena);
+        dialogBuilder.setCancelable(true).setTitle(titulo);
+        dialogBuilder.create().show();
+        }
+    
+    
+	private String getJSONdata(String url) {
+		String response = "";
+		try {
+			DefaultHttpClient httpClient = new DefaultHttpClient();
+			HttpGet method = new HttpGet(url);
+			HttpResponse httpResponse = httpClient.execute(method);
+			InputStream is = httpResponse.getEntity().getContent();
+			response = convertStreamToString(is);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return response;
+	}
+	
+	
+	private static String convertStreamToString(InputStream is) {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        StringBuilder sb = new StringBuilder();
+
+        String line = null;
+        try {
+            while ((line = reader.readLine()) != null) {
+                sb.append(line + "\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                is.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return sb.toString();
+}
+
+
+	private String parseJSONdata(String data) throws JSONException {
+		
+		
+		JSONObject jsonObj = new JSONObject(data);
+		String userData = jsonObj.getString("user");
+		JSONObject item = new JSONObject(userData);
+ 
+		String user = item.getString("user");
+		
+ 
+		return user;
+	}
+    
+    
+    
+    
+    
 }
