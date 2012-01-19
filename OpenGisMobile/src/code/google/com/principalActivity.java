c07a13df6f94bb2dc22a7bbbd9af59bf23255ce9
jugade92@gmail.com <jugade92@gmail.com@3954e759-7755-a951-c8e7-e4e3e0f8dd14>
2012-01-19 11:08:27 +0000

    Añado comentarios para que veamos como funcionan las cosas y por si alguien no entiende muy bien lo que estamos haciendo.

--- OpenGisMobile/src/code/google/com/principalActivity.java
+++ OpenGisMobile/src/code/google/com/principalActivity.java
@@ -13,11 +13,17 @@ public class principalActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu);
         
+        // Recogemos los parametros que hemos guardado en extras desde el login.
+        
         Bundle extras = getIntent().getExtras();
+        
+        // Recogemos e instanciamos los TextView que van a rellenarse con los parametros pasados
         
         TextView txtDNI = (TextView) findViewById(R.id.tvIdData);
         TextView txtNombre = (TextView) findViewById(R.id.tvNameData);
         TextView txtApellidos = (TextView) findViewById(R.id.tvSurnameData);
+        
+        // Rellenamos los TextView con los parametros guardados en extras
         
         txtDNI.setText(extras.getString("dni"));
         txtNombre.setText(extras.getString("nombre"));
