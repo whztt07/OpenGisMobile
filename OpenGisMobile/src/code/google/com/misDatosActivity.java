fbfcf59f98bd95033f7a4266d76d610b4e503e8f
jugade92@gmail.com <jugade92@gmail.com@3954e759-7755-a951-c8e7-e4e3e0f8dd14>
2012-01-19 16:04:16 +0000

    Actualizando la Activity de la configuración de los datos personales.

--- OpenGisMobile/src/code/google/com/misDatosActivity.java
+++ OpenGisMobile/src/code/google/com/misDatosActivity.java
@@ -2,12 +2,21 @@ package code.google.com;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class misDatosActivity extends Activity {
 	
 	public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.misdatos);
+        
+        EditText txtDNI = (EditText) findViewById(R.id.txtDNI); 
+        
+        Bundle extras = getIntent().getExtras();
+        
+        
+        txtDNI.setText(extras.getString("dni"));  
+        
         
 	}
 
