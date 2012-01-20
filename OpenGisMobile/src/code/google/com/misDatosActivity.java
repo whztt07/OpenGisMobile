e3fb5aee17c6db588b084e711409d880bbf64744
jugade92@gmail.com <jugade92@gmail.com@3954e759-7755-a951-c8e7-e4e3e0f8dd14>
2012-01-20 11:21:17 +0000

    Actualizada la interfaz del menú. Además ya funciona el Update al guardar los datos personales.

--- OpenGisMobile/src/code/google/com/misDatosActivity.java
+++ OpenGisMobile/src/code/google/com/misDatosActivity.java
@@ -2,6 +2,7 @@ package code.google.com;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.InputFilter;
@@ -80,13 +81,27 @@ public class misDatosActivity extends Activity {
 				
 				if(correcto==true){
 					
-					alertaMensaje("Se han modificado sus datos personales","OK");
+					//Mostramos un mensaje indicando que se actualizar‡n los datos y que debe volver a loguearse.
 					
-					//Al insertarlo volvemos a la ventana Login, haciendo que el usuario vuelva a loguearse para actualizar sus datos.
+			        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(misDatosActivity.this);
+			        dialogBuilder.setMessage("Se han modificado sus datos personales.\nEl programa le devolver‡ a la pantalla de Login.");
+			        dialogBuilder.setCancelable(false).setTitle("OK");
+			        dialogBuilder.setPositiveButton("Ok",new DialogInterface.OnClickListener() { 
+			            public void onClick(DialogInterface dialog, int arg1) { 
+			                
+							//Al insertarlo volvemos a la ventana Login, haciendo que el usuario vuelva a loguearse para actualizar sus datos.
+							
+							
+							Intent vLogin = new Intent(misDatosActivity.this, loginActivity.class);
+							startActivity(vLogin);
+			            	
+			            } 
+			        }); 
+			        dialogBuilder.create().show();
+			        
 					
 					
-					Intent vLogin = new Intent(misDatosActivity.this, loginActivity.class);
-					startActivity(vLogin);
+
 					
 					
 				}else{
@@ -104,13 +119,15 @@ public class misDatosActivity extends Activity {
 	}
 	
 	
-    public void alertaMensaje(String cadena,String titulo) {
-        
-        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(this);
-        dialogBuilder.setMessage(cadena);
-        dialogBuilder.setCancelable(false).setTitle(titulo);
+	public void alertaMensaje(String mensaje,String titulo){
+		
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(misDatosActivity.this);
+        dialogBuilder.setMessage(mensaje);
+        dialogBuilder.setCancelable(false).setTitle(titulo); 
         dialogBuilder.create().show();
-        }
-    
+		
+		
+	}
+	
 
 }
