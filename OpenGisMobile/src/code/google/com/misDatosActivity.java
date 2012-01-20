8db7d3baed8ebacf02d913805d2dc6f94d362d15
jugade92@gmail.com <jugade92@gmail.com@3954e759-7755-a951-c8e7-e4e3e0f8dd14>
2012-01-20 10:02:53 +0000

    Actualizada la ventana misdatos. Ahora ya carga los datos y puedes acceder a su edición.

--- OpenGisMobile/src/code/google/com/misDatosActivity.java
+++ OpenGisMobile/src/code/google/com/misDatosActivity.java
@@ -2,6 +2,10 @@ package code.google.com;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.InputFilter;
+import android.text.Spanned;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class misDatosActivity extends Activity {
@@ -11,12 +15,46 @@ public class misDatosActivity extends Activity {
         setContentView(R.layout.misdatos);
         
         EditText txtDNI = (EditText) findViewById(R.id.txtDNI); 
+        final EditText txtNombre = (EditText) findViewById(R.id.txtNombre);
+        final EditText txtApellidos = (EditText) findViewById(R.id.txtApellidos);
+        final EditText txtEmail = (EditText) findViewById(R.id.txtEmail);
+        final EditText txtTelefono = (EditText) findViewById(R.id.txtTlf);
+        
+        final Button cmdModificar = (Button) findViewById(R.id.cmdModificar);
+        final Button cmdGuardar = (Button) findViewById(R.id.cmdGuardar);
         
         Bundle extras = getIntent().getExtras();
         
         
         txtDNI.setText(extras.getString("dni"));  
+        txtNombre.setText(extras.getString("nombre"));
+        txtApellidos.setText(extras.getString("apellidos"));
+        txtEmail.setText(extras.getString("email"));
+        txtTelefono.setText(extras.getString("telefono"));
         
+        
+        
+        //Acciones de bot—n
+        
+        
+        cmdModificar.setOnClickListener(new View.OnClickListener(){
+
+			public void onClick(View arg0) {
+			
+				cmdModificar.setEnabled(false);
+				cmdGuardar.setEnabled(true);
+				
+				txtNombre.setEnabled(true);
+				txtApellidos.setEnabled(true);
+				txtEmail.setEnabled(true);
+				txtTelefono.setEnabled(true);
+
+				
+				
+			}
+        	
+        	
+        });
         
 	}
 
