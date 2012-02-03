94db5678ad7547b160c56d938c5e935776c1d222
jugade92@gmail.com <jugade92@gmail.com@3954e759-7755-a951-c8e7-e4e3e0f8dd14>
2012-02-03 18:19:41 +0000

    Crear Apero finalizado

--- OpenGisMobile/src/code/google/com/AperoNuevo.java
+++ OpenGisMobile/src/code/google/com/AperoNuevo.java
@@ -1,10 +1,14 @@
 package code.google.com;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 public class AperoNuevo extends Activity {
 	
@@ -14,7 +18,7 @@ public class AperoNuevo extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.crearapero);
         
-        Bundle extras = getIntent().getExtras();
+        final Bundle extras = getIntent().getExtras();
         
         final EditText txtId = (EditText) findViewById(R.id.txtIDAperoNuevo);
         final EditText txtNombre = (EditText) findViewById(R.id.txtNombreAperoNuevo);
@@ -22,8 +26,53 @@ public class AperoNuevo extends Activity {
         final EditText txtDescripcion = (EditText) findViewById(R.id.txtDescripcionAperoNuevo);
         final Spinner cbTareas = (Spinner) findViewById(R.id.cbTareasApero);
         final Button bGuardar = (Button) findViewById(R.id.cmdGuardarAperoNuevo);
+       
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(
+                this, R.array.arrayTareasID, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        cbTareas.setAdapter(adapter);
         
         txtId.setText(extras.getString("idNueva"));
+        
+        
+        bGuardar.setOnClickListener(new View.OnClickListener(){
+
+			public void onClick(View v) {
+
+				
+				String tarea = cbTareas.getSelectedItem().toString();
+				
+				tarea = tarea.substring(1,1);
+
+				String url = "http://79.108.245.167/OpenGisMobile/CrearAperoWebService.php?id="+txtId.getText()+"&nom="+txtNombre.getText()+"&tam="+txtTamanyo.getText()+"&tarea="+tarea+"&desc="+txtDescripcion.getText()+"&dni="+extras.getString("dni")+"";
+
+				url = url.replaceAll(" ","%20");
+				
+				boolean finalizado = AccesoWebService.InsertarEnWebService(url);
+				
+				if(finalizado){
+					
+					Toast tt = Toast.makeText(getApplicationContext(),getString(R.string.addToolOK),Toast.LENGTH_SHORT);
+					tt.show();
+					
+					Intent vMisProductos = new Intent(AperoNuevo.this,ProductosIconListView.class);
+					vMisProductos.putExtra("dni",extras.getString("dni"));
+					startActivity(vMisProductos);
+					
+					
+				}else{
+					
+					// No se ha conseguido
+					
+				}
+				
+			}
+        	
+        	
+        	
+        });
 	}
 
 }
