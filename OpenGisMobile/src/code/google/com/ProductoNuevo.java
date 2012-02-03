6571ebcef4c4304a96237554d9abdb3df366a772
jugade92@gmail.com <jugade92@gmail.com@3954e759-7755-a951-c8e7-e4e3e0f8dd14>
2012-02-03 14:34:37 +0000

    Actualizada la interfaz de crear producto

---
+++ OpenGisMobile/src/code/google/com/ProductoNuevo.java
@@ -0,0 +1,20 @@
+package code.google.com;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ProductoNuevo extends Activity {
+	
+	
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.crearproducto);
+        
+        
+        
+        
+        
+	}
+
+}
